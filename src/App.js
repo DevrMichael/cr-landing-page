0f8716d224139db62f8045c4a7cac7560dda6c50
DevrMichael <88589247+DevrMichael@users.noreply.github.com>
2023-02-18 15:24:29 +0100

    Refactor CSS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import data from './data/data.json'
+import data from "./data/data.json";
 
 function App() {
   return (
@@ -7,53 +7,51 @@ function App() {
       <header>
         <h1>Coding Rookie</h1>
       </header>
-      <div className="first-div">
-        <div className="gradient-border">
-          <h1>{data[0].title}</h1>
+      <div className="main-container main-header first-div">
+        <div className="main-div">
+          <h1>Get <span className="highlighted-text">Hired</span> in Under a Year</h1>
           <button>Start Now</button>
         </div>
       </div>
-      <div className="second-div">
-        <div className="second-div__content">
+      <div className="flex content-header second-div">
+        <div className="large-div">
           <h1>{data[1].title}</h1>
           <p>{data[1].content}</p>
         </div>
-        <div className="second-div__image">
+        <div className="small-div second-div__image"></div>
+      </div>
+      <div className="flex content-header third-div">
+        <div className="large-div third-div__image"></div>
+        <div className="small-div">
+          <h1>{data[2].title}</h1>
+          <p>{data[2].content}</p>
         </div>
       </div>
-      <div className="third-div">
-        <div className="third-div__image"></div>
-        <div>
-        <h1>{data[2].title}</h1>
-        <p>{data[2].content}</p>
+      <div className="flex content-header fourth-div">
+        <div className="large-div">
+          <h1>{data[3].title}</h1>
+          <p>{data[3].content}</p>
         </div>
-        </div>
-      <div className="second-div">
-      <div className="second-div__content">
-        <h1>{data[3].title}</h1>
-        <p>{data[3].content}</p>
-        </div>
-        <div className="fourth-div__image">
-        </div>
+        <div className="small-div fourth-div__image"></div>
       </div>
-      <div className="fifth-div">
+      <div className="content-header fifth-div">
         <h1>{data[4].title}</h1>
         <p>{data[4].content} </p>
       </div>
-      <div className="sixth-div">
-        <div className="gradient-border">
-        <h1>{data[5].title}</h1>
-        <button>Start Now</button>
+      <div className="main-container main-header sixth-div">
+        <div className="main-div">
+          <h1>Make Your <span className="highlighted-text">Dreams</span> of Becoming a Developer a Reality</h1>
+          <button>Start Now</button>
         </div>
       </div>
       <footer>
         <div>
           <h1>Coding Rookie</h1>
         </div>
-        <div>
-          <p>Privacy Policy</p>
+        <div className="footer-links">
+          <a href="#">Privacy Policy</a>
           <p> | </p>
-          <p>Terms of Use</p>
+          <a href="#">Terms of Use</a>
         </div>
       </footer>
     </div>
